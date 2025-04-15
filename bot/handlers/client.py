afe1f512a9bed5fd3e2107ca30a74217dd5805b3
helldiver <Shoichii>
2025-04-15 18:05:32 +0300

    правки в хэндлере покупки сертификата

--- bot/handlers/client.py
+++ bot/handlers/client.py
@@ -306,13 +306,13 @@ async def select_certificate(call: types.CallbackQuery):
                            title=f'Покупка {certificate.name}',
                            description=f'{certificate.description}',
                            provider_token=env.PAYMENT_TOKEN,
-                           start_parameter="time-machine-example",
+                           # start_parameter="time-machine-example",
                            prices=[price],
                            currency='RUB',
                            need_email=True,
                            send_email_to_provider=True,
                            payload=f'{certificate.id}',
-                           provider_data=provider_data
+                           # provider_data=provider_data
                            )
 
 
